dd14d581bbda82936dd974d15b337a8c26a47c02
HeltonsLeal <hsleal@gmail.com>
2020-12-01 23:11:48 -0300

    continua

--- app/src/main/java/com/silver7/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/com/silver7/calculadoradegorjeta/MainActivity.java
@@ -3,10 +3,13 @@ package com.silver7.calculadoradegorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.sql.Struct;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,27 +22,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        valorDaConta        = findViewById(R.id.valorDaConta);
-        seekBarPorcentagem  = findViewById(R.id.seekBarPorcentagem);
-        textPorcentagem     = findViewById(R.id.textPorcentagem);
-        valorGorjeta        = findViewById(R.id.valorGorjeta);
-        valorTotal          = findViewById(R.id.valorTotal);
 
-        seekBarPorcentagem.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textPorcentagem.setText(progress+"%");
-            }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+        valorDaConta = findViewById(R.id.valorDaConta);
+        seekBarPorcentagem = findViewById(R.id.seekBarPorcentagem);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        valorGorjeta = findViewById(R.id.valorGorjeta);
+        valorTotal = findViewById(R.id.valorTotal);
 
-            }
+        String valida = valorDaConta.getText().toString();
+        if (valida == null || valida.equals("")) {
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+            valorTotal.setText("DIGITE O VALOR DA CONTA");
 
-            }
-        });
+        }else{
+
+
+                seekBarPorcentagem.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        String vrDaConta = valorDaConta.getText().toString();
+                        Double valorConta = Double.parseDouble(vrDaConta);
+                        textPorcentagem.setText(progress + "%");
+                        valorGorjeta.setText("R$ "+ (valorConta * progress) / 100);
+                        valorTotal.setText("R$ " + (((valorConta * progress) / 100) + valorConta));
+
+
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+
+
+                });}
+
+
+
+        }
+
+
+
+
+
+    public boolean validarCampos (String vTotal) {
+
+        Boolean camposValidados = true;
+
+        if (vTotal == null || vTotal.equals("")) {
+            camposValidados = false;
+        }
+
+        return camposValidados;
+
     }
 }
